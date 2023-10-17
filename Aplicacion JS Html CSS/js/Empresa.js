a0e53e858d1fdd15008050d4e5144f30abc1a7f8
jotaMir <130723926+jotaMir@users.noreply.github.com>
2023-10-16 23:37:16 -0300

    Delete Aplicacion JS Html CSS directory

--- Aplicacion JS Html CSS/js/Empresa.js
+++
@@ -1,9 +0,0 @@
-class Empresa {
-  constructor(unUsuario, unaSenia, unNombre, pFoto, unId) {
-    this.usuario = unUsuario;
-    this.senia = unaSenia;
-    this.nombre = unNombre;
-    this.foto = pFoto;
-    this.id = unId;
-  }
-}
